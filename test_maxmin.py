90ecab6449a8de71ed3a1ef7fbe36bd9025ff341
Kamil Shafigullin <kamilshafigullin@Kamils-iMac.local>
2021-12-16 20:52:35 +0300

    l

--- test_maxmin.py
+++ test_maxmin.py
@@ -9,7 +9,7 @@ assert maxmin.get_sum([1, 7, 3]) == 11, "Должно быть 11"
 
 assert maxmin.get_mult([1, 2, 2, -1]) == -4, "Должно быть -4"
 
-assert maxmin.get_evens_count([7, 6, 3, 6, 2]) == 3, "Должно быть 3"
+assert maxmin.get_evens_count([7, 6, 3, 6, 2]) == 4, "Должно быть 3"
 
 # назначаем ограничение выполнения программы размеров в 1 минуту
 assert maxmin.get_time(maxmin.start_time) < 60, "Время выполнения программы вышло за границы допустимого"
